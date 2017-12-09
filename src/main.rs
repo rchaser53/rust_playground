86d20e5ef85dabab50fef3f251adedf71c98081f
rchaser53 <tayoshizawa29@gmail.com>
2017-12-09 23:56:53 +0900

    try to create HttpRequestHandler

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,15 @@ use std::io::{self, Write};
 use job_scheduler::{JobScheduler, Job};
 use std::time::Duration;
 
-use hyper::{Client, Method, StatusCode};
+use hyper::{Client, Body, Method, StatusCode};
+use hyper::client::{Client as HyperClient, HttpConnector};
 use tokio_core::reactor::Core;
 
 use hyper::header::{UserAgent};
 use hyper::server::{Http, Request, Response, Service};
 use hyper_tls::HttpsConnector;
 
-use futures::Stream;
-use futures::Future;
+use futures::{Stream, Future};
 
 fn main() {
     let mut sched = JobScheduler::new();
@@ -36,12 +36,25 @@ fn main() {
     }
 }
 
+struct HttpRequestHandler {
+    client: hyper::Client<HttpsConnector<HttpConnector>>
+}
+impl HttpRequestHandler {
+    fn new(core: &Core) -> Self {
+        let handle = core.handle();
+        let client = Client::configure()
+                        .connector(HttpsConnector::new(4, &handle).unwrap())
+                        .build(&handle);
+
+        HttpRequestHandler {
+            client: client
+        }
+    }
+}
+
 fn create_request_object(path: &'static str) {
     let mut core = tokio_core::reactor::Core::new().unwrap();
-    let handle = core.handle();
-    let client = Client::configure()
-                    .connector(HttpsConnector::new(4, &handle).unwrap())
-                    .build(&handle);
+    let client = HttpRequestHandler::new(&core).client;
 
     let mut req = Request::new(Method::Get, path.parse().unwrap());
     req.headers_mut().set(UserAgent::new("todo"));

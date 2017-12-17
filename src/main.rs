9a87059f043f3d5f8ac94a4ddd23e7f2c7773684
rchaser53 <tayoshizawa29@gmail.com>
2017-12-17 21:55:32 +0900

    try to use slice for return type

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use std::mem;
 
-fn main() {}
+fn main() {
+}
+
+// fn main() {
+    // let mut temp: [i16;3] = [1,2,3];
+    // let mut temp = vec![1,2,3];
+//     put_array(&mut temp);
+//     println!("{:?}", temp);
+// }
+
+// lifetimeが指定してやれるのでOK
+// fn put_array<'a>(nyan: &'a mut [i16]) -> &'a mut [i16] {
+//     nyan[1] = 13;
+//     return nyan;
+// }
 
 fn mut_and_dereference() {
     let mut a: [i16; 3] = [1 , 2, 3];
@@ -13,3 +27,7 @@ fn mut_and_dereference() {
 // fn hoge(a: &[i16]) -> [i16; 2] {
 //     [a[0], a[2]]
 // }
+
+fn head_ascii(str: &str) -> &str {
+  &str[0..1]
+}
